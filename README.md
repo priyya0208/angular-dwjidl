ff44a08c358c272488b88a8cb9d5c54893dbb64a
priyya0208 <priyya.0208@gmail.com>
2020-01-22 13:33:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dwjidl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dwjidl)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onintervalFired(firedNumber: number){
+    if(firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    }
+    else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+}
